bcc3d9380b4c9d8348960941cfcb5bab452087c0
Oriqquack <jkp8277@naver.com>
2021-12-03 16:32:28 +0900

    21/12/03 kotlin practice2

---
+++ app/src/main/java/com/example/realtest/Ktpractice(고급).kt
@@ -0,0 +1,16 @@
+package com.example.realtest
+
+//1.Lambda
+//람다식은 우리가 마치 value 처럼 다룰 수 있는 익명 함수이다.
+//1) 메소드의 파라미터로 넘겨줄수가 있다. fun maxBy(a:Int)
+//2) return 값으로 사용할 수가 있다.
+
+// 람다의 기본정의
+//val lamdaName : Type={argumentList ->codeBody}
+
+
+
+
+fun main(){
+
+}
